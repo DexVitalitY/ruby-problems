90bfa8b28571c774a745121d470f337b5cf2cad9
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-05-04 17:37:47 +0800

    PrimeNumber

---
+++ PrimeNumber.rb
@@ -0,0 +1,18 @@
+#### Prime Number
+
+#Write a function to check if a number is a prime number
+
+
+def primeNumber?(number)
+  divisor = 2
+  while ((number/2+1) > divisor)  do
+  	if (number % divisor === 0) 
+  		return false 
+  	end 
+  	divisor++
+	end
+  return true 	
+end
+
+puts primeNumber?(3)
+puts primeNumber?(6)
