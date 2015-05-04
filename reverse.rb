68a1413652f91424a468ae69e23d31c7cac13749
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-05-04 17:02:58 +0800

    Reverse.rb

---
+++ reverse.rb
@@ -0,0 +1,11 @@
+#### Reverse String
+
+# Write a function to reverse a string
+
+def reverse(string)
+	string.reverse
+end 
+
+puts reverse("Harry")
+puts reverse("My long string")
+puts reverse("HELLO THERE")
