084080ea5205f035e2c8e68226adc7670f3ff9d1
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-05-04 17:43:51 +0800

    PrimeNumber Corrected no ++ does not work in Ruby

--- PrimeNumber.rb
+++ PrimeNumber.rb
@@ -2,17 +2,16 @@
 
 #Write a function to check if a number is a prime number
 
-
-def primeNumber?(number)
+def primeNumber(number)
   divisor = 2
-  while ((number/2+1) > divisor)  do
-  	if (number % divisor === 0) 
+  while (number/2+1 > divisor) do
+  	if (number % divisor == 0) 
   		return false 
   	end 
-  	divisor++
+  	divisor += 1
 	end
   return true 	
 end
 
-puts primeNumber?(3)
-puts primeNumber?(6)
+puts primeNumber(3)
+puts primeNumber(6)
